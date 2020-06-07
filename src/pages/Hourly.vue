8bf158fb04474bbd77db88f24a74259be012a128
Berkmann18 <maxieberkmann@gmail.com>
2020-06-07 18:58:26 +0100

    Update: Better Hourly UI

--- src/pages/Hourly.vue
+++ src/pages/Hourly.vue
@@ -1,56 +1,58 @@
 <template>
   <q-page class="flex flex-center">
     <section class="fit row wrap justify-evenly items-center content-around">
-      <q-form class="col-auto self-center q-gutter-md">
-        <q-input
-          v-model="hourlyWage"
-          outlined
-          type="number"
-          prefix="£"
-          label="(Current) Hourly Wage"
-          :rules="minWageRule"
-        />
-        <q-select
-          v-model="feeMultiplier"
-          outlined
-          :options="multipliers"
-          label="Fee Multiplier"
-          transition-show="scale"
-          transition-hide="scale"
-          prefix="x"
-          :rules="fillingRule"
-        />
+      <q-card>
+        <q-card-section>
+          <q-form class="col-auto self-start q-gutter-md">
+            <q-input
+              v-model="hourlyWage"
+              outlined
+              type="number"
+              prefix="£"
+              label="(Current) Hourly Wage"
+              :rules="minWageRule"
+            />
+            <q-select
+              v-model="feeMultiplier"
+              outlined
+              :options="multipliers"
+              label="Fee Multiplier"
+              transition-show="scale"
+              transition-hide="scale"
+              prefix="x"
+              :rules="fillingRule"
+            />
 
-        <q-input
-          v-model="timeEstimate"
-          outlined
-          type="number"
-          suffix="h"
-          label="Time Estimate"
-          :rules="fillingRule"
-        />
-        <q-select
-          v-model="estimationMultiplier"
-          outlined
-          :options="multipliers"
-          label="Estimation Multiplier"
-          transition-show="scale"
-          transition-hide="scale"
-          prefix="x"
-          :rules="fillingRule"
-        />
+            <q-input
+              v-model="timeEstimate"
+              outlined
+              type="number"
+              suffix="h"
+              label="Time Estimate"
+              :rules="fillingRule"
+            />
+            <q-select
+              v-model="estimationMultiplier"
+              outlined
+              :options="multipliers"
+              label="Estimation Multiplier"
+              transition-show="scale"
+              transition-hide="scale"
+              prefix="x"
+              :rules="fillingRule"
+            />
 
-        <q-input
-          v-model="hoursPerWeek"
-          outlined
-          type="number"
-          suffix="h"
-          label="Hours Per Week (to dedicate)"
-          :rules="fillingRule"
-        />
-
-        <q-btn label="Calculate" color="accent" />
-      </q-form>
+            <q-input
+              v-model="hoursPerWeek"
+              outlined
+              type="number"
+              suffix="h"
+              label="Hours Per Week (to dedicate)"
+              :rules="fillingRule"
+            />
+          </q-form>
+        </q-card-section>
+      </q-card>
       <q-card class="col-auto self-center q-gutter-md">
         <q-card-section v-show="totalCost">
           <p>
@@ -60,7 +62,7 @@
             Your Estimated estimate: <em>{{ estimatedEstimate }} h</em>.
           </p>
           <p>
-            Total cost: <strong>£ {{ totalCost }}</strong>
+            Total cost: <strong>£ {{ totalCost }}</strong> (£ {{ totalNetCost }} net)
           </p>
           <p>
             Your completion range:
@@ -89,8 +91,8 @@ export default {
       multipliers: [1, 1.3, 1.5, 2, 3, 4],
       feeMultiplier: 1.5,
       estimationMultiplier: 1.3,
-      hourlyWage: 18, //8.2,
-      timeEstimate: 200, //0,
+      hourlyWage: 11.77, //8.2,
+      timeEstimate: 160, //0,
       hoursPerWeek: 5, //0,
       fillingRule: [(val) => !!val || 'Field is required'],
       minWageRule: [(val) => val > 8.2 || 'This is lower minimum wage for your age group is £8.20!']
@@ -105,6 +107,9 @@ export default {
     },
     totalCost() {
       return prettify(this.estimatedEstimate * this.hourlyRate, PN_CONFIG);
+    },
+    totalNetCost() {
+      return prettify(this.estimatedEstimate * this.hourlyRate * .8, PN_CONFIG);
     },
     completionRange() {
       return {
