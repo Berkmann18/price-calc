df561b39849cf6314b473077e261f8d3f00393f9
Berkmann18 <maxieberkmann@gmail.com>
2020-07-03 12:18:00 +0100

    New: Dialog component

---
+++ src/components/Dialog.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-dialog v-model="show">
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">{{ title }} Error</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">{{ message }}</q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn v-close-popup flat label="Try again" color="primary" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      required: true
+    },
+    title: {
+      type: String,
+      default: 'Operational'
+    },
+    message: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
